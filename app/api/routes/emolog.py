5aed4060f98e4c34538fc4d55e39305e0cd9913f
Atherizz <saveroathallah86@gmail.com>
2025-08-05 22:44:41 +0700

    remove db schema

--- app/api/routes/emolog.py
+++ app/api/routes/emolog.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends
-from sqlmodel import Session
-from app.db.session import get_session
-from app.core.security import verify_jwt
-from app.db.models.emolog_histories import EmologHistories, InsertHistory, GetHistoryByUserId
+# from sqlmodel import Session
+# from app.db.session import get_session
+# from app.core.security import verify_jwt
+# from app.db.models.emolog_histories import EmologHistories, InsertHistory, GetHistoryByUserId
 from app.services.detector import EmologDetector
 from datetime import datetime
 from pydantic import BaseModel
@@ -13,26 +13,12 @@ detector = EmologDetector()
 
 class TextInput(BaseModel):
     text: str
+    subdistrict_id: int
     return_all_scores: bool = False
 
 @router.post("/predict")
-def predict_emotion(input: TextInput, user_data: dict = Depends(verify_jwt),session: Session = Depends(get_session)):
+def predict_emotion(input: TextInput):
     result = detector.predict_emotion(input.text, input.return_all_scores)
 
-    history = EmologHistories(
-        user_id=user_data.id, 
-        emotion_label=result,
-        text_input=input.text,
-        subdistrict_id=1,
-        recorded_at=datetime.utcnow()
-    )
+    return {"emotion_label": result}
 
-    InsertHistory(history, session)
-
-    return {"perasaan kamu saat ini": result}
-
-@router.get("/emolog-history")
-def get_emolog_history(user_data: dict = Depends(verify_jwt), session: Session = Depends(get_session)):
-    histories = GetHistoryByUserId(user_data.id, session)
-    
-    return histories
--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = FastAPI(title="Emolog API")
 
 app.include_router(emolog.router, prefix="/api")
 
-@app.on_event("startup")
-def on_startup():
-    from app.db.session import create_db_and_tables
-    create_db_and_tables()
+# @app.on_event("startup")
+# # def on_startup():
+# #     from app.db.session import create_db_and_tables
+# #     create_db_and_tables()
